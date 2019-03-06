c2b800def96dc0c2e6a79fc17345062803f4cb2b
Bruno Kuasney <bruno.kuasney@gmail.com>
2019-03-06 09:36:52 -0300

    cnn fit

--- Convolutional_Neural_Networks/cnn.py
+++ Convolutional_Neural_Networks/cnn.py
@@ -1,32 +1,83 @@
+'''
+    1 - convolution
+    2 - Pooling
+    3 - Flattening
+    4 - Full Conection
+    5 - Fit
+'''
+
+
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import keras
+import pickle
+import os
 
-# configure logging
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(process)-5d][%(asctime)s][%(filename)-20s][%(levelname)-8s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
-logging.getLogger(__name__)
+# Part 1 - Building the CNN
 
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
 
-'''
-    Using a cnn algorithm to make churn predict with a bank daatbase
-    Output:
-        1 if the customer leaves the bank
-        0 if the customer don't leave the bank
-'''
+# Initialising the CNN
+classifier = Sequential()
 
+# Step 1 - Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+    # 32 features filters using a matrix 3x3
+    # resize image to 64x64x3 (colored image)
 
-'''
-Step 1 - Data Preprocessing
-'''
+# Step 2 - Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+    # pooling using MaxPooling to a matrix 2x2
 
-# Importing DataSet
-dataset = pd.read_csv('dataset/Social_Network_Ads.csv')
-x = dataset.iloc[: [2,3]].values
-y = dataset.iloc[: 4].values
-logging.info(x)
-logging.info(y)
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Step 3 - Flattening
+classifier.add(Flatten())
+    # turns matrix into a flat array
+
+# Step 4 - Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+    # 128 input nodes to identify characteristics
+    # 1 output (dog or cat, 0 or 1)
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    # adam optimizer. Stochastic Gradient Descent
+
+# Part 2 - Fitting the CNN to the images
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255, # pixels between 0 and 1
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64), # image resize to 64,64
+                                                 batch_size = 32, # batch
+                                                 class_mode = 'binary')
+
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64), # image resize to 64,64
+                                            batch_size = 32, # batch
+                                            class_mode = 'binary')
+
+classifier.fit_generator(training_set,
+                         steps_per_epoch = 8000, # number of images on a training set
+                         epochs = 25,
+                         validation_data = test_set,
+                         validation_steps = 2000 # number of images on a test_set
+                         )
